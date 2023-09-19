cceafae2b89acd778b76b6df9ac147d673c154df
Subhajit Mandal <108950928+Subhajit-mn@users.noreply.github.com>
2023-09-20 00:17:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function volume_sphere() {
     //Write your code here
-  
+    let r = document.getElementById("radius");
+	let volume_sphere = (4*3.14*r*r*r)/3;
+	console.log(volume);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
